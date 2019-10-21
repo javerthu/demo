d9f00f6b123b46fdc20d8750664623448b18b38c
javerthu <867216905@qq.com>
2019-10-21 16:09:25 +0800

    xpath路径爬取51job岗位和链接练习

--- Xpath example.py
+++ Xpath example.py
@@ -5,25 +5,22 @@ from lxml import etree
 '''
 用lxml来解析HTML代码
 '''
-r = requests.get('https://www.w3school.com.cn/example/xmle/books.xml')
+def getText(url):
+    try:
+        kv = {'user-agent':'Mozilla/5.0'}
+        r = requests.get(url,headers=kv,timeout=30)
+        r.raise_for_status()#状态不为200，引发异常
+        r.encoding = r.apparent_encoding
+        return r
+    except:
+        return ''
+url = 'https://search.51job.com/list/000000,000000,0000,00,9,99,python,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
+r = getText(url)
 
 print(type(r.content))
-
 # 利用etree.HTML把字符串解析成HTML文档
 html = etree.HTML(r.content)
 print(type(html))
-
-rst = html.xpath('//book')
-print(type(rst))
-print(rst)
-
-# xpath的意识是，查找带有category属性值为sport的book元素
-rst = html.xpath('//book[@category="web"]')
-print(type(rst))
-print(rst)
-
-# xpath的意识是，查找带有category属性值为sport的book元素下的year元素
-rst = html.xpath('//book[@category="web"]/price')
+rst = html.xpath("//div/p/span/a")
 for i in rst:
-    print(type(i))
-    print(i.tag, i.text)
+    print(i.xpath("@href")[0], i.text.replace(' ', ''))
